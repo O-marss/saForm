3a0b2f216a36cfcf939b3029aa488949a7f95a14
0mar-cc <omarmoh3010@gmail.com>
2024-11-14 18:27:06 +0200

    New Design without submit action

--- js/index.js
+++ js/index.js
@@ -1,54 +1,61 @@
-const form = document.getElementById('form');
-const result = document.getElementById('result');
-
+const form = document.getElementById("form");
+const result = document.getElementById("result");
 
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  const inputs = document.querySelectorAll(".form-control");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
-        form.classList.add('was-validated')
-        form.addEventListener('submit', function(e) {
+        form.classList.add("was-validated");
+        form.addEventListener("submit", function (e) {
           e.preventDefault();
           const formData = new FormData(form);
           const object = Object.fromEntries(formData);
           const json = JSON.stringify(object);
-          result.innerHTML = "Please wait..."
-        
-            fetch('https://api.web3forms.com/submit', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json'
-                    },
-                    body: json
-                })
-                .then(async (response) => {
-                    let json = await response.json();
-                    if (response.status == 200) {
-                        result.innerHTML = "Form submitted successfully";
-                    } else {
-                        console.log(response);
-                        result.innerHTML = json.message;
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                    result.innerHTML = "Something went wrong!";
-                })
+          result.innerHTML = "Please wait...";
+
+          fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: json,
+          })
+            .then(async (response) => {
+              let json = await response.json();
+              if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+              } else {
+                console.log(response);
+                result.innerHTML = json.message;
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+              result.innerHTML = "Something went wrong!";
+            })
+            .then(function () {
+              form.reset();
+              setTimeout(() => {
+                result.style.display = "none";
+              }, 3000);
+            });
         });
-      }, false)
-      
-    })
-  })();
-
-
- 
+      },
+      false
+    );
+  });
+})();
