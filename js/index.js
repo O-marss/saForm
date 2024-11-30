b3f3964bd04a569013f9a9220691a904fc5bc81b
0mar-cc <omarmoh3010@gmail.com>
2024-12-01 00:42:58 +0200

    Finished the project

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
-// (() => {
-//   "use strict";
+(() => {
+  "use strict";
 
-//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//   const forms = document.querySelectorAll(".needs-validation");
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
 
-//   // Loop over them and prevent submission
-//   Array.from(forms).forEach((form) => {
-//     form.addEventListener(
-//       "submit",
-//       (event) => {
-//         if (!form.checkValidity()) {
-//           event.preventDefault();
-//           event.stopPropagation();
-//         } else {
-//           event.preventDefault(); // Prevent form submission
-//         }
-//         form.classList.add("was-validated");
-//       },
-//       false
-//     );
-//   });
-// })();
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          event.preventDefault();
+          window.print()
+          // Prevent form submission
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
 
